c0683a2fc107f82a09f0e03a84ff577eb5e38397
sangjae <qkrtkdwo98@naver.com>
2022-05-02 17:12:23 +0900

    longest increasing partial sequence

--- dynamic_program/Longest increasing partial sequence_4.cpp
+++ dynamic_program/Longest increasing partial sequence_4.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
+#include <stack>
 using namespace std;
 
 int main() {
@@ -15,16 +16,33 @@ int main() {
 		cin >> a[i];
 	}
 	vector<int> d(n);
+	vector<int> v(n);
 	for (int i = 0; i < n; i++) {
 		d[i] = 1;
+		v[i] = -1;
 		for (int j = 0; j < i; j++) {
 			if (a[j] < a[i] && d[i] < d[j] + 1) {
 				d[i] = d[j] + 1;
+				v[i] = j;
 			}
 		}
 	}
-
+	int index = max_element(d.begin(), d.end()) - d.begin();
 	cout << *max_element(d.begin(), d.end()) << '\n';
+	
+	stack<int> st;
+	st.push(a[index]);
+	while (v[index]!=-1) {
+		
+		index=v[index];
+		st.push(a[index]);
+
+	}
+	while (!st.empty()) {
+		cout << st.top() <<' ';
+		st.pop();
+	}
+	cout << '\n';
 	return 0;
 }
 */
