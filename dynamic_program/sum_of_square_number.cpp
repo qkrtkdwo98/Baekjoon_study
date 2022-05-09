67363564b6d05c24e54ebb433772324fb3a06ce7
sangjae <qkrtkdwo98@naver.com>
2022-05-09 15:54:48 +0900

    sum_of_square_number

--- dynamic_program/sum_of_square_number.cpp
+++ dynamic_program/sum_of_square_number.cpp
@@ -9,5 +9,19 @@ int main() {
 	cin.tie(NULL);
 	cout.tie(NULL);
 
+	int n = 0;
+
+	cin >> n;
+
+	vector<int> d(n+1);
+	for (int i = 1; i <= n; i++) {
+		d[i] = i;
+		for (int j = 1; j * j <= i; j++) {
+			if (d[i] > d[i - j * j] + 1) {
+				d[i] = d[i - j * j] + 1;
+			}
+		}
+	}
+	cout << d[n];
 	return 0;
 }
