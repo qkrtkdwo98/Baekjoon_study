015f39e963c7477fefbc37aa1755dfd7e1b4cab5
sangjae <qkrtkdwo98@naver.com>
2023-01-05 21:23:54 +0900

    14499_주사위굴리기

--- 14499_주사위굴리기/14499_주사위굴리기.cpp
+++ 14499_주사위굴리기/14499_주사위굴리기.cpp
@@ -1,22 +1,42 @@
 #include <iostream>
 #include <vector>
+
+/*
+쓸데없이 너무 어렵게 생각함...
+위치는 단순히 주사위의 위치를 생각하기위해 정해준것
+매 굴림마다 변한다고 생각하면 편함
+
+예를들어 1이 영원히 같은 자리가 아니라 항상 주사위를 굴릴때 위 방향이 1이라고 생각하고
+동, 서, 남 북 방향으로 굴릴때 변화를 확인하자.
+
+vector의 초기화는 이전값을 현재값에 넣는것이 가능 쓸데없이 배열에서 for문 쓸필요 없고 vector로 바로 선언하자.
+*/
 using namespace std;
 int map[20][20];
 int N, M, x_f, y_f, K;
 
-int dx[4] = {0,0,-1,1};
-int dy[4] = {1,-1,0,0};
-//                    동 서 북 남
-int dice_dir[7][5] = {0, 0, 0, 0, 0,
-                      4, 3, 5, 2, 6,
-                      4, 3, 1, 6, 5,
-                      1, 6, 5, 2, 4,
-                      6, 1, 5, 2, 3,
-                      4, 3, 6, 1, 2,
-                      4, 3, 2, 5, 1};
-int dice[7];
+int dx[5] = {0,0,0,-1,1};
+int dy[5] = {0,1,-1,0,0};
 
+
+vector<int> dice(7); 
 vector<int> v;
+
+int diceroll(int dir) {
+    if (dir == 1) {  // 동
+        dice = { 0, dice[4],dice[2],dice[1],dice[6],dice[5],dice[3] }; // 위, 북, 동, 서, 남, 아래
+    }
+    else if (dir == 2) {  // 서
+        dice = { 0, dice[3],dice[2],dice[6],dice[1],dice[5],dice[4] }; // 위, 북, 동, 서, 남, 아래
+    }
+    else if (dir == 3) {  // 북
+        dice = { 0, dice[5],dice[1],dice[3],dice[4],dice[6],dice[2] }; // 위, 북, 동, 서, 남, 아래
+    }
+    else if (dir == 4) {  // 남
+        dice = { 0, dice[2],dice[6],dice[3],dice[4],dice[1],dice[5] }; // 위, 북, 동, 서, 남, 아래
+    }
+    return 0;
+}
 
 int main() {
     scanf("%d %d %d %d %d", &N, &M, &x_f, &y_f, &K);
@@ -37,38 +57,22 @@ int main() {
     int dice_next = 1;
 
     for (int i = 0; i < K; i++) {
-        int next = v[i]-1;
+        int next = v[i];
         int nx = x + dx[next];
         int ny = y + dy[next];
         if (nx < N && nx >= 0 && ny < M && ny >= 0) {
-            dice_next = dice_dir[dice_now][next];
-            dice_now = dice_next;
-            printf("%d\n", dice_now);
+            diceroll(next);
             if (map[nx][ny] == 0) {
-                map[nx][ny] = dice[dice_dir[dice_now][4]];
+                map[nx][ny] = dice[6];
             }
             else {
-                dice[dice_dir[dice_now][4]] = map[nx][ny];
+                dice[6] = map[nx][ny];
                 map[nx][ny] = 0;
             }
-            printf("%d\n", dice[dice_now]);
+            printf("%d\n", dice[1]);
             x = nx;
             y = ny;
         }
-        for (int k = 0; k < N; k++) {
-            for (int j = 0; j < M; j++) {
-                printf("%d ", map[k][j]);
-            }
-            printf("\n");
-        }
-        printf("\n");
     }
-
-/*
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < M; j++) {
-            printf("%d ", map[i][j]);
-        }
-    }*/
     return 0;
 }
